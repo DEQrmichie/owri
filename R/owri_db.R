f4962a000b7961dfdbf7f5ec505aa335d78d0acd
DEQrmichie <michie.ryan@deq.state.or.us>
2022-08-03 20:56:30 -0700

    owri_db function
    New function to access the most recent database directly from the R package.

---
+++ R/owri_db.R
@@ -0,0 +1,18 @@
+#' Current OWRI Database
+#'
+#' Returns the path to the most recent SQLite OWRI database export included with this R pacakge.
+#'
+#' @param owri.db Optional. owri SQLite database file name. Default is the
+#'        most recent version. 'OWRIDbExport_011022.db'.
+#'
+#' @keywords owri, raw_data
+#' @export
+
+owri_db <- function(owri.db = "OWRIDbExport_011022.db") {
+
+  owri.db <- system.file("extdata", owri.db, package = "owri", mustWork = TRUE)
+
+  return(owri.db)
+
+}
+
