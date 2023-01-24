6314cd77df2d34e25df1e8382ec6609c283fd31c
DEQrmichie <ryan.michie@deq.oregon.gov>
2023-01-24 13:59:22 -0800

    update to OWRI 011023

--- R/owri_db.R
+++ R/owri_db.R
@@ -3,12 +3,12 @@
 #' Returns the path to the most recent SQLite OWRI database export included with this R pacakge.
 #'
 #' @param owri.db Optional. owri SQLite database file name. Default is the
-#'        most recent version. 'OWRIDbExport_011022.db'.
+#'        most recent version. 'OWRIDbExport_011023.db'.
 #'
 #' @keywords owri, raw_data
 #' @export
 
-owri_db <- function(owri.db = "OWRIDbExport_011022.db") {
+owri_db <- function(owri.db = "OWRIDbExport_011023.db") {
 
   owri.db <- system.file("extdata", owri.db, package = "owri", mustWork = TRUE)
 
