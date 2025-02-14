9909c9b020bc4b6aef9ef33091eed93eac4b2483
DEQrmichie <65993167+DEQrmichie@users.noreply.github.com>
2025-02-13 16:24:27 -0800

    update to OWRI db 010825

--- R/owri_db.R
+++ R/owri_db.R
@@ -1,14 +1,16 @@
 #' Current OWRI Database
 #'
-#' Returns the path to the most recent SQLite OWRI database export included with this R pacakge.
+#' Returns the path to the most recent SQLite OWRI database export included with this R package.
+#' The original OWRI database published by the Oregon Watershed Enhancement Board can be found here:
+#' https://hub.oregonexplorer.info/pages/water-planning-enhancing-watersheds-in-oregon#TheOWRIDatabaseandGISdata
 #'
 #' @param owri.db Optional. owri SQLite database file name. Default is the
-#'        most recent version. 'OWRIDbExport_011023.db'.
+#'        most recent version. 'OwriDbExport_010825.db'.
 #'
 #' @keywords owri, raw_data
 #' @export
 
-owri_db <- function(owri.db = "OWRIDbExport_011023.db") {
+owri_db <- function(owri.db = "OwriDbExport_010825.db") {
 
   owri.db <- system.file("extdata", owri.db, package = "owri", mustWork = TRUE)
 
